d646eda69e5ef3723f1dfa7ba28024957a121cf7
John Doe <omniman793@gmail.com>
2025-12-01 19:41:31 +0700

    Resolved merge conflicts

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -3,9 +3,8 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import ProductList from './pages/ProductList'
 import axios from 'axios';
-import './App.css'
-
-const createProduct = async () => {
+import Contact from "./pages/Feedback";
+const find = async () => {
   try {
     const response = await axios.get('http://localhost:8000/api/products');
 
@@ -21,31 +20,12 @@ const createProduct = async () => {
 function App() {
   find();
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+      <Route path="/feedback" element={<Contact />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/list" element={<ProductList />} />
+    </Routes>
   )
 }
 
 export default App
-
-
